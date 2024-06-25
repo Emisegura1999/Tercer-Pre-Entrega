bcfa40fbd1b9ef44f35e3b26cf7d3155cfec5415
Emisegura1999 <emisegura99@gmail.com>
2024-06-25 09:05:32 -0300

    mas correciones

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const path = require('path');
 const passport = require("passport");
 
+require("./database.js");
+
 const initializePassport = require("./config/passport.config.js");
+const configObject = require("./config/env.config.js");
+
 const auth = require("./middleware/authmiddleware.js");
-const configObject = require("./config/envConfig.js");
-const path = require('path');
-require("../database.js");
+const ErrorManager = require("./middleware/errorsMiddleware.js");
+const brotliCompression = require("./middleware/brotlyCompression.js");
 
 const SocketManager = require("./sockets/socketmanager.js");
 
@@ -17,27 +19,32 @@ const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/carts.router.js");
 const viewsRouter = require("./routes/views.router.js");
 const userRouter = require("./routes/user.router.js");
+const testRouter = require("./routes/test.router.js");
 
 const app = express();
 const PORT = configObject.server.port;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-initializePassport();
 app.use(cookieParser());
+app.use(passport.initialize());
 app.use(auth);
+
+initializePassport();
+brotliCompression();
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
- 
+
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/api/users", userRouter);
+app.use("/api/mockingProducts", testRouter);
 app.use("/", viewsRouter);
+app.use(ErrorManager);
+
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Server connected http://localhost:${PORT}`);
